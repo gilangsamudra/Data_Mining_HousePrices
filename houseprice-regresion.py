e70cf6a25f0707a43b3df76612315f7f4ca4101a
Gilang <gilang.samudrakuswana@gmail.com>
2020-04-20 18:02:16 +0700

    final
    do simple regression using ols function
    do regression plot using seaborn function
    do histogram plot using pandas and matplotlib

--- houseprice-regresion.py
+++ houseprice-regresion.py
@@ -1,8 +1,41 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 20 17:29:39 2020
+# Bismillah
+# Import all necessary libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from statsmodels.formula.api import ols
 
-@author: GT60
-"""
+# Load dataset
+df = pd.read_csv('D:/Phyton Code/Data_Mining_HousePrices/kc_house_data.csv')
 
+# # Check data quality
+# df.info()
+# df.head()
+# df.tail()
+# df.describe()
+# df.isnull().any()
+# df.dtypes
 
+# Create a figure object
+fig = plt.figure(figsize=(12, 6))
+sqft = fig.add_subplot(121)
+cost = fig.add_subplot(122)
+
+sqft.hist(df.sqft_living, bins=80)
+sqft.set_xlabel('Ft^2')
+sqft.set_title('Histogram of Housing Prices')
+
+cost.hist(df.price, bins=80)
+cost.set_xlabel('Price ($)')
+cost.set_title("Histogram of Housing Prices")
+
+# Do a simple single variable regresion calculation
+m = ols('price ~ sqft_living', df).fit()
+print(m.summary())
+
+# Do a multivariate regresion calculation
+mm = ols('price ~ sqft_living + bedrooms + condition + grade', df).fit()
+print(mm.summary())
+
+sns.jointplot(x='sqft_living', y='price', data=df, kind='reg', fit_reg=True,
+              height=7)
