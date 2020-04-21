91d53380a2cf19434ff33f9a772ce6e252c81a68
Gilang <gilang.samudrakuswana@gmail.com>
2020-04-21 11:31:13 +0700

    1

--- houseprice-classification.py
+++ houseprice-classification.py
@@ -1,8 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 21 11:01:19 2020
+# Bismillah
+# Classification
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
-@author: GT60
-"""
+import sklearn
+from sklearn import cluster
 
-
+# Bagian 1 -  expolratory data analysis
+df = pd.read_csv('D:/Phyton Code/Contoh dari Github/from-data-with-love-master/data/faithful.csv')
+df.columns = ['eruptions', 'waiting']
+# Bagian 2 - scatter plot
+plot = plt.figure(dpi=150, figsize=(12, 6))
+plt.scatter(df.eruptions, df.waiting)
+plt.title('Old Faithful Data Scatterplot')
+plt.xlabel('Leng of eruption (minutes)')
+plt.ylabel('Time between eruptions (minutes)')
