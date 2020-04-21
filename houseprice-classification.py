48ea093ed6b0bafc4601c2d85221ad96199793b6
Gilang <gilang.samudrakuswana@gmail.com>
2020-04-21 11:43:20 +0700

    2

--- houseprice-classification.py
+++ houseprice-classification.py
@@ -11,9 +11,11 @@ from sklearn import cluster
 # Bagian 1 -  expolratory data analysis
 df = pd.read_csv('D:/Phyton Code/Contoh dari Github/from-data-with-love-master/data/faithful.csv')
 df.columns = ['eruptions', 'waiting']
-# Bagian 2 - scatter plot
+
+# Bagian 2 - pandas scatter plot API
 plot = plt.figure(dpi=150, figsize=(12, 6))
-plt.scatter(df.eruptions, df.waiting)
-plt.title('Old Faithful Data Scatterplot')
-plt.xlabel('Leng of eruption (minutes)')
-plt.ylabel('Time between eruptions (minutes)')
+fig = plot.add_subplot()
+fig.scatter(data=df, x='eruptions', y='waiting')
+fig.set_title('Old Faithful Data Scatterplot')
+fig.set_xlabel('Length of eruption (minutes)')
+fig.set_ylabel('Time between eruptions (minutes)')
